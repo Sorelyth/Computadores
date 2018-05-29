f816679f4002fc1bafecf73042ae1fb2f5dfa4c6
Sorelyth <sore.vp@gmail.com>
2018-05-28 21:29:51 -0500

    Falta detalles y estilos

--- app/src/main/java/sore/computadores/CrearComputadores.java
+++ app/src/main/java/sore/computadores/CrearComputadores.java
@@ -43,8 +43,55 @@ public class CrearComputadores extends AppCompatActivity {
         SO.setAdapter(adpS);
 
 
+        //fotos = new ArrayList<Integer>();
+        //fotos.add(R.drawable.images);
+        //fotos.add(R.drawable.images2);
+        //fotos.add(R.drawable.images3);
+        //fotos.add(R.drawable.images4);
 
     }
 
+    public String obtenerDato(Spinner spn){
+        String cadena = "";
+        cadena = spn.getSelectedItem().toString();
+        return cadena;
+    }
+
+    public void guardar(View v){
+        String marca, color, tipo,so, id;
+        int ram, foto;
+        foto = Datos.fotoAleatoria(fotos);
+        marca = obtenerDato(Marca);
+        ram  = Integer.parseInt(Ram.getText().toString());
+        color = obtenerDato(Color);
+        tipo = obtenerDato(Tipo);
+        so = obtenerDato(SO);
+        id = Datos.getId();
+
+        Computador C = new Computador(id, marca, color, tipo, so, ram, foto);
+        C.guardar();
+        Snackbar.make(v, getResources().getString(R.string.mensaje_guardado_exitoso), Snackbar.LENGTH_SHORT)
+                .setAction("Action", null).show();
+        limpiar();
+    }
+
+    public void limpiar(View v){
+        limpiar();
+    }
+
+    public void limpiar(){
+        Marca.setSelection(0);
+        Ram.setText("");
+        Color.setSelection(0);
+        Tipo.setSelection(0);
+        SO.setSelection(0);
+    }
+
+    public void onBackPressed(){
+        finish();
+        Intent i = new Intent(CrearComputadores.
+                this,Main.class);
+        startActivity(i);
+    }
 
 }
